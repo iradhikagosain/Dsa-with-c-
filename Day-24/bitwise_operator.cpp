7e87effc33b434f43aab94afc01b403565dcf5c8
Radhika Gosain <iradhikagosain@gmail.com>
2025-10-13 21:31:00 +0530

    Day-24

---
+++ Day-24/bitwise_operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int num1,num2;
+    cout<<"enter num 1:";
+    cin>>num1;
+    cout<<"\n enter num 2:";
+    cin>>num2;
+    cout<<"\n BITWISE OPERATOR ANSWER:";
+    cout<<(num1 ^ num2);/*AND(&)   OR(|)   XOR(^)*/
+    return 0;
+
+}
---
+++ Day-24/complement_shift.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int num1,num2;
+    cout<<"enter num 1:";
+    cin>>num1;
+    cout<<"\n enter num 2:";
+    cin>>num2;
+    cout<<"\n BITWISE SHIFT ANSWER:";
+    cout<<(num1>>num2);
+    
+    cout<<"\n BITWISE OPERATOR ANSWER:";
+    cout<<(~num1);
+    cout<<(~4);
+    return 0;
+    
+
+}
+
